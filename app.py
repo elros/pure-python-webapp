58c05208aedf8aaa6e9efc44e2b2c72a110b3c0d
Artem Maslovskiy <em.arcamenel@gmail.com>
2017-07-04 17:48:49 +0300

    delete test views

--- app.py
+++ app.py
@@ -12,24 +12,6 @@ db = database.FeedbackDatabase(
 url_resolver = UrlResolver()
 
 
-@url_resolver.get('/comments/')
-def comments_view(request):
-    return UrlResolver.Response(
-        status='200 OK',
-        headers=[],
-        body='It worked!',
-    )
-
-
-@url_resolver.get('/comment/(?P<id>\d+)')
-def comment_detail_view(request, id):
-    return UrlResolver.Response(
-        status='200 OK',
-        headers=[],
-        body='Comment #{} details'.format(id)
-    )
-
-
 def wsgi_handler(request, start_response):
     response = url_resolver.get_response(request)
     response.headers.append(('Content-Length', str(len(response.body))))
