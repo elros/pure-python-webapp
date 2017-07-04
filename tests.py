f3c05e1cd102dd1c7b0f15a563b9aabca8f619a6
Artem Maslovskiy <em.arcamenel@gmail.com>
2017-07-04 17:47:29 +0300

    add UrlResolver tests

--- tests.py
+++ tests.py
@@ -2,6 +2,7 @@
 import unittest
 
 import util
+from url_resolver import UrlResolver
 
 
 class TestUtilities(unittest.TestCase):
@@ -28,14 +29,65 @@ class TestUtilities(unittest.TestCase):
 
 class TestUrlResolver(unittest.TestCase):
 
-    def test_normal_urlconf(self):
-        pass
-
     def test_default_handler(self):
-        pass
+        url_resolver = UrlResolver()
+
+        fake_request = {
+            'PATH_INFO': '/fake/',
+            'REQUEST_METHOD': 'GET',
+        }
+
+        fake_response = url_resolver.get_response(fake_request)
+        default_response = url_resolver.get_default_handler()(fake_request)
+
+        self.assertEqual(fake_response, default_response)
+
+    def test_normal_urlconf(self):
+        url_resolver = UrlResolver()
+
+        @url_resolver.get('/user/(?P<id>\d+)')
+        def user_getter(request, id):
+            return UrlResolver.Response(
+                status='200 OK',
+                headers=[],
+                body='details for user #%s' % (id),
+            )
+
+        response = url_resolver.get_response(
+            request={
+                'PATH_INFO':  '/user/42',
+                'REQUEST_METHOD': 'GET',
+            }
+        )
+
+        self.assertEqual(response.body, 'details for user #42')
 
     def test_urlconf_miss(self):
-        pass
+        url_resolver = UrlResolver()
+
+        @url_resolver.get('/user/(?P<id>\d+)')
+        def user_getter(request, id):
+            return UrlResolver.Response(
+                status='200 OK',
+                headers=[],
+                body='details for user #%s' % (id),
+            )
+
+        miss_response = url_resolver.get_response(
+            request={
+                'PATH_INFO': '/missing-path/42',
+                'REQUEST_METHOD': 'GET',
+            }
+        )
+
+        default_response = url_resolver.get_default_handler()(
+            request={
+                'PATH_INFO': '/fake/',
+                'REQUEST_METHOD': 'GET',
+            }
+        )
+
+        self.assertEqual(miss_response, default_response)
 
 
 class TestDatabase(unittest.TestCase):
