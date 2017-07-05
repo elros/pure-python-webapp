8e5d11354acc6fe121069375c97b96ff6336128f
Artem Maslovskiy <em.arcamenel@gmail.com>
2017-07-05 17:15:21 +0300

    make file path cross-platform

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import os.path
 
 # Development server parameters
 DEV_SERVER_HOST = 'localhost'
@@ -7,5 +8,5 @@ DEV_SERVER_PORT = 8051
 # Sqlite database settings
 DATABASE = {
     'SQLITE_FILE_PATH': 'feedback_app.db',
-    'DUMP_FILE_PATH': 'sql/load_db.sql',
+    'DUMP_FILE_PATH': os.path.join('sql', 'load_db.sql'),
 }
