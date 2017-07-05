8fe9439e91471ac9f9374c2a751e72722552ff78
Artem Maslovskiy <em.arcamenel@gmail.com>
2017-07-05 14:48:20 +0300

    delete unused to_quoted_identifiers_list function

--- util.py
+++ util.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
-def to_quoted_identifiers_list(items, quotes='\'', sep=', '):
-    escaped = []
-
 def to_quoted_list(items, quotes='\'', sep=', '):
     quoted = lambda s: quotes + str(s) + quotes
     return sep.join(quoted(item) for item in items)
